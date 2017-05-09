d7da2e95a0cc4f5f3bb3c75bccf74bfa92ff2e41
vyle3696 <vyle3696@gmail.com>
2017-05-09 12:18:54 +0700

    nothing

--- QLNT/DataLayer/DataAccessHelper.cs
+++ QLNT/DataLayer/DataAccessHelper.cs
@@ -21,11 +21,9 @@ namespace QLNT.DataLayer
         #region Init properties
         public DataAccessHelper()
         {
-<<<<<<< HEAD
+
             String connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QLNT;Data Source=DESKTOP-HVQQFLB\VYLE";
-=======
-            String connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QLNT;Data Source=DESKTOP-A7DLQ1N\HONGHANH";
->>>>>>> origin/master
+
 
             con = new SqlConnection(connectionString);
         }
