7b07e920dd071e700587bcf76cd05eb1fb03b85f
phamnutuyetan <14520010@gm.uit.edu.vn>
2017-06-04 18:04:29 +0700

    add MonAnBLL.cs và NguyenLieuTungMonBLL.cs

---
+++ QLNT/BusinessLayer/MonAnBLL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.DataLayer;
+using QLNT.Entities;
+using System.Data;
+
+namespace QLNT.BusinessLayer
+{
+    class MonAnBLL
+    {
+        public static void ThemMonAn(MonAn monAn)
+        {
+            MonAnDAL.ThemMonAn(monAn);
+        }
+
+        public static void XoaMonAn(string maMon)
+        {
+            MonAnDAL.XoaMonAn(maMon);
+        }
+
+        public static void CapNhatMonAn(MonAn monAn)
+        {
+            MonAnDAL.CapNhatMonAn(monAn);
+        }
+
+        public static DataTable LayDanhSachMonAn()
+        {
+            return MonAnDAL.LayDanhSachMonAn();
+        }
+       
+        public static string SinhMaTuDong()
+        {
+            string maCuoi = MonAnDAL.LayMaCuoi().Trim();
+
+            //chưa có phiếu mua nào trong CSDL
+            if (maCuoi == "")
+            {
+                return "MAMA000001";
+            }
+
+            //xoa 4 chữ cái đầu chuỗi và tăng số đếm lên 1
+            int maTiepTheo = int.Parse(maCuoi.Remove(0, 4)) + 1;
+            string maTuDong = maTiepTheo.ToString().Trim();
+
+            while (maTuDong.Length < 6)
+            {
+                maTuDong = "0" + maTuDong;
+                maTuDong.Trim();
+            }
+
+            return "MAMA" + maTuDong;
+        }
+
+       
+    }
+}
---
+++ QLNT/BusinessLayer/NguyenLieuTungMonBLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.DataLayer;
+using QLNT.Entities;
+using System.Data;
+
+namespace QLNT.BusinessLayer
+{
+    class NguyenLieuTungMonBLL
+    {
+        public static void ThemNLTungMon(NguyenLieuTungMon nlTungMon)
+        {
+            NLTungMonDAL.ThemNLTungMon(nlTungMon);
+        }
+
+        public static void CapNhatNL(NguyenLieuTungMon nlTungMon)
+        {
+            NLTungMonDAL.CapNhatNL(nlTungMon);
+        }
+
+        public static DataTable LayDanhSachNLTungMon(string maMon)
+        {
+            return NLTungMonDAL.LayDanhSachNLTungMon(maMon);
+        }
+
+        public static void XoaNLTungMon(string maMon, string maNL)
+        {
+            NLTungMonDAL.XoaNLTungMon(maMon, maNL);
+        }
+
+        public static bool KiemTraMaNL(string maMon, string MaNL)
+        {
+            return NLTungMonDAL.KiemTraMaNL(maMon, MaNL);
+        }
+    }
+}
