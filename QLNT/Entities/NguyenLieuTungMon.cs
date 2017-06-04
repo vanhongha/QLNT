9d228dcdc6d8ffe029e82327bf162da09bd93201
phamnutuyetan <14520010@gm.uit.edu.vn>
2017-06-04 18:12:14 +0700

    thêm NguyenLieuTungMon.cs trong Entinities

---
+++ QLNT/Entities/NguyenLieuTungMon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNT.Entities
+{
+    class NguyenLieuTungMon 
+    {
+        private string _maMon;
+        private string _maNL;
+        private int _soLuong;
+
+        public string MaMon
+        {
+            get
+            {
+                return _maMon;
+            }
+
+            set
+            {
+                _maMon = value;
+            }
+        }
+
+        public string MaNL
+        {
+            get
+            {
+                return _maNL;
+            }
+
+            set
+            {
+                _maNL = value;
+            }
+        }
+
+        public int SoLuong
+        {
+            get
+            {
+                return _soLuong;
+            }
+
+            set
+            {
+                _soLuong = value;
+            }
+        }
+    }
+}
